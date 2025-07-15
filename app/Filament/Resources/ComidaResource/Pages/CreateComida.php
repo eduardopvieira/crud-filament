71619ff982118bf685aab352f64baa7b52370eed
Eduardo Vieira <eduardo.vieira@alunos.ufersa.edu.br>
2025-07-15 11:49:06 -0300

    redirect apos criacao/edicao

--- app/Filament/Resources/ComidaResource/Pages/CreateComida.php
+++ app/Filament/Resources/ComidaResource/Pages/CreateComida.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateComida extends CreateRecord
 {
     protected static string $resource = ComidaResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
